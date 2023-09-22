617d45a12bc7c5f618a64dfbcfe3798a38788ba3
Origho-precious <Origho9@gmail.com>
2023-09-22 21:30:42 +0100

    feat: Higher-order Array methods solutions to No.2 & 3

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numArr) {
+	return numArr
+		.filter((num) => num % 2 === 0)
+		.map((num) => num * num)
+		.reduce((total, num) => total + num, 0);
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,14 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+	const totalAmount = products.reduce(
+		(total, curProduct) => total + curProduct.price * curProduct.quantity,
+		0
+	);
+
+	const tax = totalAmount * (taxRate / 100);
+
+	const totalSalesWithTax = totalAmount + tax;
+
+	return parseFloat(totalSalesWithTax.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
