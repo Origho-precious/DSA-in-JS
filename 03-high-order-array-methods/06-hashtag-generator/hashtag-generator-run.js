ab1933ca29118bf9c3ad1903a13696c1ee45e90d
Origho-precious <Origho9@gmail.com>
2023-11-05 00:34:25 +0100

    chore: hasgtag generator solved

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator-run.js
@@ -1,5 +1,6 @@
 const generateHashtag = require('./hashtag-generator');
 
 const result = generateHashtag('hello world');
+// const result = generateHashtag('');
 
 console.log(result);
--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
-function generateHashtag() {}
+function generateHashtag(text) {
+	if (!text || text?.length > 140) return false;
+
+	const newText = text.split(" ").map((word) => capitalize(word));
+
+	newText.unshift("#");
+
+	return newText.join("");
+}
+
+const capitalize = (word) => {
+	return word.slice(0, 1).toUpperCase() + word.slice(1);
+};
 
 module.exports = generateHashtag;
