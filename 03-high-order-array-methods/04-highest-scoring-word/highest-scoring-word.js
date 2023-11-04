ab0d243b9e51fc0a0525d882313ba51561453cc2
Origho-precious <Origho9@gmail.com>
2023-11-04 20:29:41 +0100

    chore: 04-11-23

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,28 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+	const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+	const words = str.split(" ");
+
+	const wordsObj = {};
+
+	words.forEach((word) => {
+		wordsObj[word] = word
+			.split("")
+			.reduce(
+				(acc, curVal) => alphabets.indexOf(curVal.toLowerCase()) + 1 + acc,
+				0
+			);
+	});
+
+	let biggest = words[0];
+
+	for (let word in wordsObj) {
+		if (wordsObj[word] > wordsObj[biggest]) {
+			biggest = word;
+		}
+	}
+
+	return biggest;
+}
 
 module.exports = highestScoringWord;
