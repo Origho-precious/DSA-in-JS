3834ed57455db73ad8cd6c3f92bc8d0b453b8b6f
Origho-precious <Origho9@gmail.com>
2023-11-04 23:59:31 +0100

    chore: Valid Anagrams solved

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,28 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+	if (str1.length !== str2.length) return false;
+
+	const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+	/**
+	 * 1. Rearrange the string
+	 * 2. Compare them
+	 */
+
+	const sortedStr1 = str1
+		.split("")
+		.map((letter) => alphabets.indexOf(letter))
+		.sort((a, b) => a - b)
+		.map((num) => alphabets[num])
+		.join("");
+
+	const sortedStr2 = str2
+		.split("")
+		.map((letter) => alphabets.indexOf(letter))
+		.sort((a, b) => a - b)
+		.map((num) => alphabets[num])
+		.join("");
+
+	return sortedStr1 === sortedStr2
+}
 
 module.exports = validAnagrams;
