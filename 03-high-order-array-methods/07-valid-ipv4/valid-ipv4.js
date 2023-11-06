a91d389f645d36d188dd787543df51edd7ca216c
Origho-precious <Origho9@gmail.com>
2023-11-06 19:22:36 +0100

    feat: valid ipv4 solved

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,24 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ipAddress) => {
+	const octets = ipAddress.split(".");
+
+	if (octets.length !== 4) {
+		return false;
+	}
+
+  const map = octets.map((octet) => {
+    
+		if (
+			Number(octet).toString() !== octet ||
+			Number(octet) > 255 ||
+			Number(octet) < 0
+		) {
+			return false;
+		}
+
+		return true;
+	});
+
+	return !map.includes(false);
+};
 
 module.exports = isValidIPv4;
