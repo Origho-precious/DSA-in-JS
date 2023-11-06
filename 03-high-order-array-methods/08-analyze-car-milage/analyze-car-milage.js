bd6ed6295cb6f36c7db11cf70251504087287925
Origho-precious <Origho9@gmail.com>
2023-11-06 19:47:56 +0100

    feat: analyse Mileage solved

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,24 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+	const totalMileage = cars.reduce((acc, curCar) => acc + curCar.mileage, 0);
+	const averageMileage = totalMileage / cars.length;
+
+	const mileageArray = cars.map((car) => parseInt(car.mileage));
+
+	const highestMileageCarIndex = mileageArray.indexOf(
+		Math.max(...mileageArray)
+	);
+
+	const lowestMileageCarIndex = mileageArray.indexOf(Math.min(...mileageArray));
+
+	const highestMileageCar = cars[highestMileageCarIndex];
+	const lowestMileageCar = cars[lowestMileageCarIndex];
+
+	return {
+		totalMileage,
+		averageMileage,
+		highestMileageCar,
+		lowestMileageCar,
+	};
+}
 
 module.exports = analyzeCarMileage;
