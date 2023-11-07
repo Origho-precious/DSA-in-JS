bca1c26702991810c07bc366a97bf557ad9ec4b8
Origho-precious <Origho9@gmail.com>
2023-11-07 23:48:17 +0100

    feat: password-validator solved

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,43 @@
-function validatePassword() {}
+/**
+ * A Regex will be more optimal 
+ * but this exercise is for higher order array methods
+ */
+
+function validatePassword(password) {
+	if (
+		password.length < 8 ||
+		!containsUppercaseLetter(password) ||
+		!containsLowercaseLetter(password) ||
+		!containsNumber(password)
+	) {
+		return false;
+	}
+
+	return true;
+}
+
+const containsUppercaseLetter = (password) => {
+	const uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	return Array.from(password).some((letter) =>
+		uppercaseAlphabets.includes(letter)
+	);
+};
+
+const containsLowercaseLetter = (password) => {
+	const lowercaseAlphabets = "abcdefghijklmnoprstuvwxyz";
+
+	return Array.from(password).some((letter) =>
+		lowercaseAlphabets.includes(letter)
+	);
+};
+
+const containsNumber = (password) => {
+	const numbers = "0123456789";
+
+	return Array.from(password).some((letter) =>
+		numbers.includes(letter.toString())
+	);
+};
 
 module.exports = validatePassword;
