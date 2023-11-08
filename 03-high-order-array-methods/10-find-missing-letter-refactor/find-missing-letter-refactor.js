17defac45f778e5a72b9c658fbc138488fb55440
Origho-precious <Origho9@gmail.com>
2023-11-08 23:27:25 +0100

    feat: finding missing letter solved

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,20 @@
-function findMissingLetter() {}
+function findMissingLetter(letterArray) {
+	const alphabets = Array.from(
+		"abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	);
+
+	const firstLetterIndex = alphabets.indexOf(letterArray[0]);
+	const lastLetterIndex = alphabets.indexOf(
+		letterArray[letterArray.length - 1]
+	);
+
+	const expectedArray = alphabets.slice(firstLetterIndex, lastLetterIndex);
+
+	const missingLetter = expectedArray.find(
+		(letter) => !letterArray.includes(letter)
+	);
+
+	return missingLetter;
+}
 
 module.exports = findMissingLetter;
