3447706b5148a6b4db554ce83298d392364df06d
Origho-precious <Origho9@gmail.com>
2023-11-16 21:21:49 +0100

    feat: recursion intro exercise solved

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,14 @@
-function countDown() {}
+function countDown(num) {
+	if (num <= 0) {
+		console.log("All done!");
+		return;
+	}
+
+	console.log(num);
+
+	num--;
+
+	countDown(num);
+}
 
 module.exports = countDown;
